2f67e06582c8ba73a2119fe212c66ede50282732
“sunny <sunny.tanwar@mail.vinove>
2021-09-22 14:58:34 +0530

    water

--- hungry.py
+++ hungry.py
@@ -2,4 +2,6 @@ hungry=input("are you hungry :")
 if hungry==("yes"):
     print("Go to the kitchen & find something to eat")
 else:
-    print("Keep working")
+    thirsty=input("are you thirsty")
+    if thirsty==("yes"):
+        print("be hydrated bro, take a break and drink some water")
